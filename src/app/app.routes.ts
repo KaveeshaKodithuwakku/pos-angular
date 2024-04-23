6d2708e89a2604b7eca86a7c9d513bb0379d7aa8
DELL <k.kaveesha95@gmail.com>
2024-04-23 14:17:09 +0530

    Completed Product save, update and delete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {CustomerContextComponent} from "./components/customer-context/customer-context.component";
+import {CustomerNewComponent} from "./components/customer-context/inner/customer-new/customer-new.component";
+import {CustomerUpdateComponent} from "./components/customer-context/inner/customer-update/customer-update.component";
+import {CustomerAllComponent} from "./components/customer-context/inner/customer-all/customer-all.component";
+import {DashboardHomeComponent} from "./components/dashboard-home/dashboard-home.component";
+import {ProductContextComponent} from "./components/product/product-context/product-context.component";
+import {ProductNewComponent} from "./components/product/product-context/inner/product-new/product-new.component";
+import {
+  ProductUpdateComponent
+} from "./components/product/product-context/inner/product-update/product-update.component";
+import {ProductAllComponent} from "./components/product/product-context/inner/product-all/product-all.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',redirectTo:'/dashboard/home',pathMatch:'full'},
+  {
+    path:'dashboard',component:DashboardComponent, children:[
+      {path:'',redirectTo:'/dashboard/home',pathMatch:'full'},
+      {path:'home',component:DashboardHomeComponent},
+      {
+        path:'customers',component:CustomerContextComponent, children:[
+          {path:'',redirectTo:'/dashboard/customers/new',pathMatch:'full'},
+          {path:'new',component:CustomerNewComponent},
+          {path:'update/:id',component:CustomerUpdateComponent},
+          {path:'list',component:CustomerAllComponent}
+        ]
+      },
+      {
+        path:'products',component:ProductContextComponent, children:[
+          {path: '',redirectTo: '/dashboard/products/new',pathMatch: 'full'},
+          {path: 'new',component:ProductNewComponent},
+          {path: 'update/:id',component: ProductUpdateComponent},
+          {path: 'list',component: ProductAllComponent}
+        ]
+      }
+    ]
+  }
+
+];
